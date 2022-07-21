0658c08afcc6a3fdb40c8c013ca2280136e4caa4
Adam Kowalewski <akowalewski@deloittece.com>
2022-07-21 11:10:32 +0200

    Task 1,2,3 and begin of Task 4

--- src/app/cats/cats-data.service.ts
+++ src/app/cats/cats-data.service.ts
@@ -13,31 +13,46 @@ export class CatsDataService {
       name: 'Puszek',
       ageMonths: 8,
       breed: 'maine coon',
-      picUrl: 'puszek1.jpg'
+      picUrl: 'puszek1.jpg',
+      vaccinated: true,
+      funFact: "Dobrze skacze po dachach",
+      isReserved: false
     },
     {
       name: 'Mruczek',
       ageMonths: 36,
       breed: 'brytyjczyk',
-      picUrl: 'mruczek1.jpg'
+      picUrl: 'mruczek1.jpg',
+      vaccinated: true,
+      funFact: "Jest wysoki",
+      isReserved: true
     },
     {
       name: 'Kiciek',
       ageMonths: 52,
       breed: 'perski',
-      picUrl: 'kiciek.jpg'
+      picUrl: 'kiciek.jpg',
+      vaccinated: true,
+      funFact: "Jest szczupły",
+      isReserved: false
     },
     {
       name: 'Kłaczek',
       ageMonths: 81,
       breed: 'brytyjczyk',
-      picUrl: 'klaczek.jpg'
+      picUrl: 'klaczek.jpg',
+      vaccinated: true,
+      funFact: "Ma duże łapki",
+      isReserved: true
     },
     {
       name: 'Pimpuś',
       ageMonths: 6,
       breed: 'dachowiec',
-      picUrl: 'pimpus.jpg'
+      picUrl: 'pimpus.jpg',
+      vaccinated: false,
+      funFact: "Ma długie wąsy",
+      isReserved: false
     }
   ];
 
